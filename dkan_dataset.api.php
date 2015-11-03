4834393efdde2532376def4a20fa17ab85a2b9ca
Dan Feder <dan@nucivic.com>
2015-11-03 21:43:55 +0000

    NuCivic/internal#879 Redo previews logic to make formats suggested but not enabled by default

--- dkan_dataset.api.php
+++ dkan_dataset.api.php
@@ -15,26 +15,19 @@
  * Add to or alter the array of external previews available to DKAN Dataset
  *
  * @param array $previews
- *   An associative array of preview types containing the elements "name" (the
- *   publicly-viewable name for this preview type), "link_callback" and"format".
- *   "link_callback" provides a function that will be called and passed the
- *   dataset node object to generate the actual link URL for the preview. See
- *   the default previdews defined in
- *   dkan_dataset_teaser_get_external_previews() for examples. After all alter
- *   hooks have run, the array will be parsed by
- *   dkan_dataset_teaser_get_external_previews() and a "url" element added.
- *   "Format" sets the default resource format that the preview will be enabled
- *   for; this can be overridden in the admin settings form at
- *   admin/dkan/dataset_preview.
+ *   An associative array of preview types. See
+ *   dkan_dataset_teaser_get_external_previews() for documentation on array
+ *   elements.
  *
  * @param $node
  *   The dataset node object.
  */
-function hook_dkan_dataset_external_previews_alter(&$previews, $node) {
+function hook_dkan_dataset_external_previews_alter(&$previews) {
   $previews['third_party_service'] = array(
-    'name' => 'Third-Party Mapping Service',
+    'name' => t('Third-Party Mapping Service'),
+    'description' => t('A longer description of the preview.')
     'link_callback' => 'mymodule_preview_link',
-    'format' => array('geojson'),
+    'suggested_format' => array('geojson'),
   );
 }
 

a2ceb136005d5f4fa895d83d7c219a463859dce9
Dan Feder <dafeder@gmail.com>
2016-09-26 17:15:15 -0400

    Revert dkan-module-init.sh to 7.x-1.x branch

--- dkan-module-init.sh
+++ dkan-module-init.sh
@@ -3,7 +3,7 @@
 DKAN_MODULE=`ls *.info | cut -d'.' -f1`
 
 # DKAN branch or tag to use.
-DKAN_VERSION="3494-move-odfe-fields"
+DKAN_VERSION="7.x-1.x"
 
 COMPOSER_PATH="$HOME/.config/composer/vendor/bin"
 
